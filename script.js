45ef969845e76c04e72b54625cb25ae426e7dfdb
skrithi <44930218+skrithi@users.noreply.github.com>
2019-07-01 14:39:46 -0400

    create script.js
    load index from elasticsearch

---
+++ script.js
@@ -0,0 +1,36 @@
+var app = angular.module('app', ['ngVis']);
+
+app.controller('MainCtrl', ['$scope', 'VisDataSet',
+
+  function($scope, VisDataSet) {
+
+    $scope.onSelect = function(items) {
+      // debugger;
+      alert('select');
+    };
+
+    $scope.onClick = function(props) {
+      //debugger;
+      alert('Click');
+    };
+
+    $scope.onDoubleClick = function(props) {
+      // debugger;
+      alert('DoubleClick');
+    };
+
+    $scope.rightClick = function(props) {
+      alert('Right click!');
+      props.event.preventDefault();
+    };
+    
+    $scope.options = {
+      autoResize: true,
+      height: '800',
+      width: '100%'
+    };
+    
+    $scope.data = {}; 
+    
+  }
+]);
